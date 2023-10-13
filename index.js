cf2465428e8087b57b1afc2abc42af02612d8212
Shatterdest <73318855+Shatterdest@users.noreply.github.com>
2023-10-13 13:15:38 -0400

    fuck css

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
 function insertRGBBox(rgb) {
-    const rgbBox = document.createElement("div");
-    rgbBox.classList.add("rgb-box");
+  if (checkInput(rgb.red) && checkInput(rgb.green) && checkInput(rgb.blue)) {
+    const rgbBox = addBoxContents(rgb);
     rgbBox.style.backgroundColor = `rgb(${rgb.red}, ${rgb.green}, ${rgb.blue})`;
     DOMSelectors.container[0].appendChild(rgbBox);
+  }
+    else {
+        alert("Please enter a number between 0 and 255");
+    }
+}
+
+function checkInput(input) {
+  if (input >= 0 && input <= 255) {
+    return true;
+  }
+  return false;
+}
+
+function createBox() {
+  const rgbBox = document.createElement("div");
+  rgbBox.classList.add("rgb-box");
+  return rgbBox;
+}
+
+function addBoxContents(rgb) {
+  const box = createBox();
+  const rgbText = document.createElement("p");
+  rgbText.classList.add("rgb-text");
+  rgbText.textContent = `RGB: ${rgb.red}, ${rgb.green}, ${rgb.blue}`;
+  const rgbButton = document.createElement("button");
+  rgbButton.classList.add("rgb-button");
+  rgbButton.textContent = "Delete Box";
+  rgbButton.addEventListener("click", function () {
+    box.remove();
+  });
+  box.appendChild(rgbText);
+  box.appendChild(rgbButton);
+  return box;
 }
 
 const DOMSelectors = {
-    form: document.getElementById("rgbInput"),
-    red: document.getElementById("red"),
-    green: document.getElementById("green"),
-    blue: document.getElementById("blue"),
-    container: document.getElementsByClassName("container"),
-}
+  inputform: document.getElementById("rgbInput"),
+  red: document.getElementById("red"),
+  green: document.getElementById("green"),
+  blue: document.getElementById("blue"),
+  container: document.getElementsByClassName("container"),
+};
 
-DOMSelectors.form.addEventListener('submit', function(e){
-    e.preventDefault();
-    
-    const color = {
-        red: DOMSelectors.red.value,
-        green: DOMSelectors.green.value,
-        blue: DOMSelectors.blue.value
-    }
-    console.log(`RGB entered: ${color.red}, ${color.green}, ${color.blue}`)
-    console.log(DOMSelectors.container)
-    insertRGBBox(color);
-    DOMSelectors.form.reset();
+DOMSelectors.inputform.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const color = {
+    red: DOMSelectors.red.value,
+    green: DOMSelectors.green.value,
+    blue: DOMSelectors.blue.value,
+  };
+  console.log(`RGB entered: ${color.red}, ${color.green}, ${color.blue}`);
+  console.log(DOMSelectors.container);
+  insertRGBBox(color);
+  DOMSelectors.inputform.reset();
 });
